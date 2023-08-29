b5d93b6ec0eb405c5f5965f39119c65a7918a879
olachinkei <kei.kyoto.be.amb@gmail.com>
2023-08-30 08:48:35 +0900

    all

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,6 +1,6 @@
-import glob
 import os
 import wandb
+import json
 from datasets import load_dataset
 from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM
 
@@ -8,45 +8,36 @@ os.environ["WANDB_ENTITY"] = "reviewco"
 os.environ["WANDB_PROJECT"] = "Autocompletion with evaluation"
 os.environ["WANDB_USERNAME"] = "keisuke-kamata"
 
-tokenizer = AutoTokenizer.from_pretrained("facebook/opt-125m")
-data = load_dataset("databricks-dolly-15k",split="train")
+config = {
+    "base_model":"facebook/opt-125m",
+    "data_set":"databricks/databricks-dolly-15k",
+}
+
+with wandb.init(config=config, job_type="data_prep") as run:
+    config = wandb.config
+
+    tokenizer = AutoTokenizer.from_pretrained(config.base_model)
+    data = load_dataset(config.data_set,split="train")
+    
+    data_train_test = data.train_test_split(test_size=0.2)
+    data_train_valid = data_train_test["train"]
+    data_train_valid = data_train_valid.train_test_split(test_size=0.2)
+    data_train = data_train_valid["train"]
+    data_valid = data_train_valid["test"]
+    data_test = data_train_test["test"]
 
 
+    data_train_list = data_train.to_list()
+    data_valid_list = data_valid.to_list()
+    data_test_list = data_test.to_list()
+    with open('raw_data/train_data.json', 'w') as f:
+        json.dump(data_train_list, f)
+    with open('raw_data/valid_data.json', 'w') as f:
+        json.dump(data_valid_list, f)
+    with open('raw_data/test_data.json', 'w') as f:
+        json.dump(data_test_list, f)
 
-
-
-data = data.map(lambda samples: tokenizer(samples['text']), batched=True)
-
-INITIAL_UPLOAD_DATE = "20230826"
-train_shuffled = data["train"].shuffle(seed=42)
-
-total_train_samples = len(train_shuffled)
-train_data_size = int(0.9 * total_train_samples)
-valid_data_size = total_train_samples - train_data_size
-
-train_data = train_shuffled.select(range(0, train_data_size))
-valid_data = train_shuffled.select(range(train_data_size, total_train_samples))
-test_data = data["test"].shuffle(seed=42)
-
-train_data.to_pandas().to_parquet(f"instruction_data/train/{INITIAL_UPLOAD_DATE}.parquet", index=False)
-valid_data.to_pandas().to_parquet(f"instruction_data/valid/{INITIAL_UPLOAD_DATE}.parquet", index=False)
-test_data.to_pandas().to_parquet(f"instruction_data/test/{INITIAL_UPLOAD_DATE}.parquet", index=False)
-
-with wandb.init() as run:
-    artifact = wandb.Artifact('instruction', type='dataset')
-    artifact.add_dir("instruction_data/")
-    run.log_artifact(artifact, aliases=["production"])
-
-for file in glob.glob("instruction_data/*.parquet"):
-    os.remove(file)
-
-train_data = train_shuffled.select(range(0, 200))
-valid_data = train_shuffled.select(range(200, 300))
-
-train_data.to_pandas().to_parquet(f"instruction_data/train/{INITIAL_UPLOAD_DATE}.parquet", index=False)
-valid_data.to_pandas().to_parquet(f"instruction_data/valid/{INITIAL_UPLOAD_DATE}.parquet", index=False)
-
-with wandb.init() as run:
-    artifact = wandb.Artifact('instruction', type='dataset')
-    artifact.add_dir("instruction_data/")
-    run.log_artifact(artifact, aliases=["downsampled"])
+    
+    artifact = wandb.Artifact('data', type='dataset')
+    artifact.add_dir("raw_data/")
+    run.log_artifact(artifact, aliases=["raw"])
---
+++ src/wandb/run-20230829_072942-1xqpo8hn/files/code/evaluation.py
@@ -0,0 +1,253 @@
+#!pip install lm-eval==0.3.0 -qqq
+import subprocess
+
+def execute_command(command):
+    result = subprocess.run(command, capture_output=True, text=True, shell=True)
+    if result.returncode != 0:
+        print(f"Error executing command: {' '.join(command)}")
+        print(result.stderr)
+    else:
+        print(result.stdout)
+#execute_command("git clone https://github.com/EleutherAI/lm-evaluation-harness")
+#execute_command("pip install -e ./lm-evaluation-harness/. -qqq")
+execute_command("pip install lm-eval 0.3.0 -qqq")
+
+import ctranslate2
+import glob
+import os
+import pandas as pd
+import peft
+import random
+import timeit
+import urllib
+import torch
+import json
+import os
+from datasets import load_dataset
+import lm_eval
+from lm_eval import evaluator, utils
+import wandb
+from peft import get_peft_model, LoraConfig, TaskType
+from peft import PeftModel, PeftConfig
+import wandb.apis.reports as wr
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM, pipeline
+
+os.environ["WANDB_PROJECT"] = "Autocompletion with evaluation"
+os.environ["WANDB_ENTITY"] = "reviewco"
+os.environ["WANDB_USERNAME"] = "keisuke-kamata"
+os.environ["WANDB_LOG_MODEL"] = "checkpoint"
+os.environ["WANDB_WATCH"] = "gradients"
+config = {
+    "MODEL_NAME":"Finetuned-Review-Autocompletion",
+    "BASE_MODEL":"facebook/opt-125m",
+    "MODEL_REGISTRY":"Instruction-tuned-model",
+}
+
+PROMPT_NO_INPUT_FORMAT = """Below is an instruction that describes a task. Write a response that appropriately completes the request.
+### Instruction:
+{instruction}
+### Response"""
+
+PROMPT_WITH_INPUT_FORMAT = """Below is an instruction that describes a task. Write a response that appropriately completes the request.
+### Instruction:
+{instruction}
+Input:
+{context}
+### Response"""
+
+
+config = {
+    "BASE_MODEL":"facebook/opt-125m",
+    "MODEL_REGISTRY":"Instruction-tuned-model",
+}
+
+def get_completion_responses_batch(prompts, base_llm,production_model, stag_model, tokenizer):
+    # Get completions for each model in batches
+    original_completions = get_huggingface_completion_batch(prompts, base_llm,tokenizer)
+    production_completions = get_huggingface_completion_batch(prompts, production_model, tokenizer)
+    stagaing_completions = get_huggingface_completion_batch(prompts, staging_model, tokenizer)
+    responses = []
+    for opt, ft in zip(opt_completions, ft_completions):
+        responses.append({
+            "Original": original_completions,
+            "Staging (finetuned)": production_completions,
+            "Production (finetuned)": production_completions
+        })
+    df = pd.DataFrame(responses)
+    df.insert(0, "prompt", prompts)
+    return df
+
+def get_huggingface_completion_batch(prompts, model, tokenizer):
+    generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
+    responses = generator(prompts, max_new_tokens=50)
+    completions = []
+    for i, prompt in enumerate(prompts):
+        full_output = responses[i][0]["generated_text"]
+        output = full_output[len(prompt):] if full_output.startswith(prompt) else full_output
+        completions.append(output.strip())
+    return completions
+
+
+def get_latency_df(prompts, num_prompts, base_llm, production_model, staging_model, tokenizer):
+  prompts = prompts.to_list()[:num_prompts]
+  original_time = timeit.timeit(lambda: get_huggingface_completion_batch(prompts, base_llm, tokenizer), number=1)
+  production_time = timeit.timeit(lambda: get_huggingface_completion_batch(prompts, production_model, tokenizer), number=1)
+  staging_time = timeit.timeit(lambda: get_huggingface_completion_batch(prompts, staging_model,tokenizer), number=1)
+
+  return pd.DataFrame({
+    "Model": ["Original","Staging (finetuned)","Production (finetuned)"],
+    f"Latency for {num_prompts} Reviews": [original_time, production_time,staging_time],
+  })
+
+
+with wandb.init(config=config,job_type="model_evaluation") as run:
+    config = wandb.config
+    # load -------------------------------
+    base_llm = AutoModelForCausalLM.from_pretrained(config.BASE_MODEL, device_map="auto", torch_dtype=torch.float16)
+    tokenizer = AutoTokenizer.from_pretrained(config.BASE_MODEL, local_files_only=True)
+    ## staging model
+    staging_model = wandb.use_artifact(f'{os.environ["WANDB_ENTITY"]}/model-registry/{config.MODEL_REGISTRY}:staging')
+    staging_path = staging_model.download()
+    staging_model = PeftModel.from_pretrained(base_llm, staging_path,torch_dtype=torch.float16)
+    staging_model = staging_model.merge_and_unload()
+    ## production model
+    production_model = wandb.use_artifact(f'{os.environ["WANDB_ENTITY"]}/model-registry/{config.MODEL_REGISTRY}:production')
+    production_path = production_model.download()
+    production_model = PeftModel.from_pretrained(base_llm, staging_path,torch_dtype=torch.float16)
+    production_model = production_model.merge_and_unload()
+    
+    # evaluation harness -------------------------------
+    
+    #tasks = ["arc_easy","hellaswag","squad2","drop","sciq"]
+    tasks = ["sciq"]
+
+    results = lm_eval.evaluator.simple_evaluate(
+        model=staging_model,
+        tasks=tasks,
+        batch_size=32,
+        num_fewshot=3,
+        device="cuda"  
+    )
+    table_contents = []
+    table_contents.append(run.id)
+#    table_contents.append(results["results"]["arc_easy"]["acc"])
+#    table_contents.append(results["results"]["hellaswag"]["acc"])
+#    table_contents.append(results["results"]["squad2"]["f1"])
+    table_contents.append(results["results"]["sciq"]["acc"])
+#    table_contents.append(results["results"]["drop"]["f1"])
+    table = wandb.Table(columns=["run_id"]+tasks,
+                                data=[table_contents])
+
+    # evaluation to test-------------------------------
+    tokenizer = AutoTokenizer.from_pretrained(config.BASE_MODEL)
+    artifact = run.use_artifact(f'{os.environ["WANDB_ENTITY"]}/{os.environ["WANDB_PROJECT"]}/data:production')
+    artifact_dir = artifact.download()
+    test_data = load_dataset('json', data_files=artifact_dir+'/test_data.json',split="train")
+
+    prompts = []
+    for i in range(0,10):
+        if test_data[i]["context"]=="":
+            source_text = PROMPT_NO_INPUT_FORMAT.format_map(test_data[i])
+        else:
+            source_text = PROMPT_WITH_INPUT_FORMAT.format_map(test_data[i])
+        prompts.append(source_text)
+        
+
+    run.log({
+        "Evaluation metric": table,
+        "Validation Responses": get_completion_responses_batch(prompts, base_llm,production_model, stag_model, tokenizer),
+        "Model Latencies": get_latency_df(prompts, num_prompts, base_llm, production_model, staging_model, tokenizer)
+    })
+    run.log_code()
+
+    report = wr.Report(
+        project=os.environ["WANDB_PROJECT"],
+        entity=os.environ["WANDB_ENTITY"],
+        title='Model Evaluation: Autocompletion Model',
+        description="Data and sample predictions to evaluate the staging candidate model for our review autocompletion algorithm."
+    )
+
+    report.width = "fluid"
+
+    runsets = [wr.Runset(
+        os.environ["WANDB_ENTITY"],
+        os.environ['WANDB_PROJECT']
+        )]
+
+    report.blocks = [
+        wr.TableOfContents(),
+        wr.H1("Report Overview"),
+        wr.P(
+            "This report contains information to evaluate whether potential staging models "
+            "should be moved to production. Model Registry admins can use the view of the "
+            "Model Registry at the end of this report to move a staging model into production, "
+            "using a Webhook automation."
+        ),
+        wr.Spotify(spotify_id="7KAveXwQ5xzdHT6GDlNIBu"),
+        wr.MarkdownBlock("May this staging model earn 5 stars 🙏."),
+        wr.HorizontalRule(),
+    ]
+
+    pg = wr.PanelGrid(
+        runsets=runsets,
+        panels=[
+        wr.ScalarChart(
+            title="Current Min Eval Loss",
+            metric="eval/loss",
+            groupby_aggfunc="min",
+            font_size="large"),
+
+        wr.ScalarChart(
+            title="Current Min Train Loss",
+            metric="train/loss",
+            groupby_aggfunc="min",
+            font_size="large"),
+
+        wr.ScalarChart(
+            title="Longest Runtime (sec)",
+            metric="Runtime",
+            groupby_aggfunc="max",
+            font_size="large"),
+
+        wr.LinePlot(x='Step',
+                    y=['eval/loss'],
+                    smoothing_factor=0.8,
+                    layout={'w': 24, 'h': 9})
+        ]
+    )
+
+    report.blocks += [wr.H1("Key Metrics"), pg]
+
+    pg = wr.PanelGrid(
+        runsets=runsets,
+        panels=[
+            wr.WeavePanelSummaryTable("Evaluation metrics", layout={'w': 24, 'h': 12}),
+        ])
+
+    report.blocks += [wr.H1("Evaluation metric"), pg]
+
+    pg = wr.PanelGrid(
+        runsets=runsets,
+        panels=[
+            wr.WeavePanelSummaryTable("Model Latencies", layout={'w': 24, 'h': 12}),
+        ])
+
+    report.blocks += [wr.H1("Latency Data for Models"), pg]
+
+    
+
+    pg = wr.PanelGrid(
+        runsets=runsets,
+        panels=[
+            wr.WeavePanelSummaryTable("Validation Responses", layout={'w': 24, 'h': 12}),
+        ])
+
+    report.blocks += [wr.H1("Sample Predictions"), pg]
+
+    report.blocks += [wr.H1("Autocompletion Model in Model Registry"), wr.WeaveBlockArtifact(os.environ["WANDB_ENTITY"], "model-registry", MODEL_NAME)]
+    report.save()
+
+    report_creation_msg = f"Report to review: {urllib.parse.quote(report.url, safe=r'/:')}"
+    print(report_creation_msg)
+
+    wandb.alert("New Staging Model Evaluated", report_creation_msg)
